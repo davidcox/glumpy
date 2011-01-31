e0a381ec988a464f8f1f93c9af24689f531558b6
Nicolas.Rougier@gmail.com <Nicolas.Rougier@gmail.com>
2011-01-31 17:32:12 +0100

    Use setup.py proposal by lev.givon (see bug #2)

--- demos/demo-fps.py
+++ demos/demo-fps.py
@@ -6,32 +6,25 @@
 # Distributed under the terms of the BSD License. The full license is in
 # the file COPYING, distributed as part of this software.
 #-----------------------------------------------------------------------------
-import ctypes
-import numpy, glumpy
+import glumpy
 import OpenGL.GL as gl
 import OpenGL.GLUT as glut
 
 window = glumpy.Window(512,512)
-Z = numpy.random.random((32,32)).astype(numpy.float32)
-I = glumpy.Image(Z, interpolation='nearest', cmap=glumpy.colormap.Grey)
-text, t0, t, frames = 'Computing FPS...', 0, 0, 0
+t0, t, frames = 0, 0, 0
 
-@window.timer(30.0)
-def timer(dt):
+
+@window.event
+def on_idle(dt):
     global text, t, t0, frames
-
     t += dt
-    frames = frames + 1
+    frames += 1
     if t-t0 > 5.0:
         fps = float(frames)/(t-t0)
-        text = 'FPS: %.2f (%d frames in %.2f seconds)' % (fps, frames, t-t0)
-        frames,t0 = 0, t
+        print 'FPS: %.2f (%d frames in %.2f seconds)' % (fps, frames, t-t0)
+        frames, t0 = 0, t
     window.clear()
 
-    gl.glColor(1,1,1,1)
-    gl.glRasterPos(1,window.height-11)
-    glut.glutBitmapString(glut.GLUT_BITMAP_8_BY_13, text)
-    window.draw()
-
+print 'Computing FPS...'
 window.mainloop()
 
--- setup.py
+++ setup.py
@@ -1,56 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #-----------------------------------------------------------------------------
-# Copyright (C) 2009-2010  Nicolas P. Rougier
+# Copyright (C) 2009,2010,2011  Nicolas P. Rougier
 #
 # Distributed under the terms of the BSD License. The full license is in
 # the file COPYING, distributed as part of this software.
 #-----------------------------------------------------------------------------
-from os.path import join
 import os
-import sys
+from distutils.core import setup
 
-# The following is more or less random copy/paste from numpy.distutils ...
-import setuptools
-from distutils.errors import DistutilsError
-from numpy.distutils.core import setup
 from common import *
 
-def configuration(parent_package='',top_path=None, package_name=DISTNAME):
+if __name__ == "__main__":
     if os.path.exists('MANIFEST'):
         os.remove('MANIFEST')
     write_info(os.path.join("glumpy", "info.py"))
     write_version(os.path.join("glumpy", "version.py"))
     if os.path.exists(os.path.join("doc", "src")):
         write_version(os.path.join("doc", "src", "glumpy_version.py"))
-    pkg_prefix_dir = 'glumpy'
-    from numpy.distutils.misc_util import Configuration
-    config = Configuration(None,parent_package,top_path,
-        namespace_packages = ['glumpy'],
-        version     = build_fverstring(),
-        maintainer  = MAINTAINER,
-        maintainer_email = MAINTAINER_EMAIL,
-        description = DESCRIPTION,
-        license = LICENSE,
-        url = URL,
-        download_url = DOWNLOAD_URL,
-        long_description = LONG_DESCRIPTION)
-    config.set_options(
-            ignore_setup_xxx_py=True,
-            assume_default_configuration=True,
-            delegate_options_to_subpackages=True,
-            quiet=True,
-            )
-    config.add_subpackage(DISTNAME)
-    return config
 
-if __name__ == "__main__":
-    setup(configuration=configuration,
-          name=DISTNAME,
-          install_requires=INSTALL_REQUIRE,
-          namespace_packages=['glumpy'],
+    setup(name=DISTNAME,
+          version=build_fverstring(),
+          description=DESCRIPTION,
+          long_description = LONG_DESCRIPTION,
+          maintainer= MAINTAINER,
+          maintainer_email=MAINTAINER_EMAIL,
+          url=URL,
+          download_url=DOWNLOAD_URL,
+          license = LICENSE,
           packages=['glumpy', 'glumpy.atb', 'glumpy.shader'],
-          include_package_data = True,
           package_data={'glumpy': ['shader/*.txt']},
-          zip_safe=False,
           classifiers=CLASSIFIERS)
